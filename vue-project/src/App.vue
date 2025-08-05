9bf18396b4ff6880c52701eb9db4de1b1ffd319d
Tushti Patel <tushtipatel1015@gmail.com>
2025-08-05 09:21:57 -0400

    Vue Externship Project
    I just completed GE Aerospace's Explore Digital Technology on Forage. In the simulation I:
    Completed a Vue.js UI development simulation for the Frontend Development Team, demonstrating proficiency in Vue SFC Playground.
    Applied Vue.js skills by adding features like headings, images, and dynamic variable displays. Implemented a compute button for efficient user interactions.
    Drafted clear technical requirements for a feature, suggesting optimal order times for plane parts.
    Explored strategies to align technical specifications with business requirements, ensuring effective solutions.

---
+++ vue-project/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Compute Product')
+const factor1 = ref(0)
+const factor2 = ref(0)
+const product = ref(0)
+const computedClicked = ref(false)
+
+function computeProduct(){
+  product.value = factor1.value*factor2.value
+  computedClicked.value = true
+}
+
+function reset(){
+  product.value = 0
+  factor1.value = 0
+  factor2.value = 0
+  computedClicked.value = false
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <img src="https://cdn.theforage.com/vinternships/companyassets/ay2tsYxaTif7Nt6z7/AA4Bnq2tJHALwE8cg/1680718790218/pexels-photo-14197334.jpeg" alt="Plane View"/>
+  <br />
+  <p>FACTOR 1: {{factor1}}</p>
+  <p>FACTOR 2: {{factor2}}</p>
+  <input v-model="factor1" /> x <input v-model="factor2" /> =
+  
+  <br />
+  <br />
+
+    <div v-if="!computedClicked">
+      <button @click="computeProduct">Compute Product</button>
+    </div>
+    <div v-else>
+      <p>Product: {{product}}</p>
+      <button @click="reset">Reset</button>
+    </div>
+</template>
